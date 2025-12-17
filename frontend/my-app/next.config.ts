891c48ed231471715d061f2aa1fe558c626d8ef2
Francis Ferrando <francisferrando@hotmail.com>
2025-12-17 13:45:54 -0300

    cambios

--- frontend/my-app/next.config.ts
+++ frontend/my-app/next.config.ts
@@ -1,17 +1,7 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    if (process.env.NODE_ENV === 'development') {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'http://localhost:5140/api/:path*', // Tu backend local
-        },
-      ];
-    }
-    return [];
-  },
+  output: "export",
 };
 
 module.exports = nextConfig;
