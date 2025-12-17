782429cd12adbb19519dc2b81c3c32507ff0ec88
Francis Ferrando <francisferrando@hotmail.com>
2025-12-17 14:47:38 -0300

    cambios

--- frontend/my-app/next.config.ts
+++ frontend/my-app/next.config.ts
@@ -1,7 +1,8 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+output: 'export', // <--- ESTA LÍNEA ES LA QUE CREA LA CARPETA 'out'
+  trailingSlash: true,
 
 };
 
